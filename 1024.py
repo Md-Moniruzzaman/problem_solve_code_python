7793bc96e66e62e823952bec3df7bd0a1f01b40d
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-01-23 11:12:29 +0600

    1024 encryption

---
+++ 1024.py
@@ -0,0 +1,13 @@
+# beecrowd | 1024
+# Encryption
+
+def encription(text):
+   
+    print(text)
+    
+
+n = int(input().strip())
+
+for i in range(0, n):
+    text = input().split()
+    encription(text)
